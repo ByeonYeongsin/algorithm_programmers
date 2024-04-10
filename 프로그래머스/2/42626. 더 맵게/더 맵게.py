775724356fd97bcd012ce713f13a136d16a7a64e
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2024-04-10 13:16:50 +0900

    [level 2] Title: 더 맵게, Time: 1700.84 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,35 +1,14 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
 import heapq as hq
 
 def solution(scoville, K):
-
-    hq.heapify(scoville)
     answer = 0
-    while True:
-        first = hq.heappop(scoville)
-        if first >= K:
-            break
-        if len(scoville) == 0:
+    hq.heapify(scoville)
+    while scoville[0] < K:
+        a = hq.heappop(scoville)
+        if len(scoville) >= 1:
+            b = hq.heappop(scoville)
+        else:
             return -1
-        second = hq.heappop(scoville)
-        hq.heappush(scoville, first + second*2)
-        answer += 1  
-
+        hq.heappush(scoville, a+b*2)
+        answer+=1
     return answer
